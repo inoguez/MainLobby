6739d877d13ae124d61452859df8480438a8c704
2GramsIn <sk8ian100@gmail.com>
2022-07-26 17:41:51 -0500

    New Methods Added: createItem - to Gui class and make it static, openInventory - No uses, fillInv - to fill spaces no used

--- src/main/java/org/crafttorch/Gui.java
+++ src/main/java/org/crafttorch/Gui.java
@@ -4,35 +4,47 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
+import java.util.List;
 
 
 public class Gui implements InventoryHolder {
-    Handler plug;
-    ConfigurationSection getConfigForGUI;
+    private final Handler plug;
     private final Inventory inv;
 
     public Gui(Handler plug, int size, String title) {
         this.plug = plug;
-        this.getConfigForGUI = plug.getCustomConfig().getConfigurationSection("GUI");
-        this.inv = Bukkit.createInventory(null, size, title);
-        getConfigForGUI.getKeys(false).forEach(key ->{
-            String mat = plug.getCustomConfig().getString("GUI." + key + ".material_fill");
-            for (int x = 0; x < size; x++){
-                ItemStack item = plug.getItems().createItem(ChatColor.BLACK + "", Material.valueOf(mat), null);
-                inv.setItem(x,item);
+        this.inv = Bukkit.createInventory(null, size, Handler.format(title));
+
+    }
+
+    public void fillInv(ConfigurationSection GUI, String mat){
+        GUI.getKeys(false).forEach(key ->{
+            for (int x = 0; x < getInventory().getSize(); x++){
+                ItemStack item = createItem(ChatColor.BLACK + "", Material.matchMaterial(mat), null);
+                getInventory().setItem(x,item);
             }
         });
     }
+    public static ItemStack createItem(String name, Material mat, List<String> lore){
+        ItemStack item = new ItemStack(mat, 1 );
+        ItemMeta meta = item.getItemMeta();
+        assert meta != null;
+        meta.setDisplayName(Handler.format(name));
+        meta.setLore(Handler.formatList(lore));
+        item.setItemMeta(meta);
+        return item;
+    }
 
-    //Method createItem
-
-    //Method message
+    public void openInventory(final HumanEntity ent) {
+        ent.openInventory(inv);
+    }
 
     @Override
     public @NotNull Inventory getInventory() {
